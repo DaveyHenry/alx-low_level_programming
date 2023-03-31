0498e648a4369d28a653c239d6dfbc3bdafef436
David Henry Boakye-Yeboah <dhenryboakyeyeboah@gmail.com>
2023-03-30 19:25:17 -0700

    Fixing errors

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,49 +1,46 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * infinite_add - Adds two numbers stored as strings
- * @n1: First number to add
- * @n2: Second number to add
- * @r: Buffer to store the result
- * @size_r: Size of the buffer
- * Return: Pointer to the result, or 0 if the result can't be stored in r
+ * infinite_add - Adds two numbers
+ *
+ * @n1: The first number to add
+ * @n2: The second number to add
+ * @r: The buffer to store the result in
+ * @size_r: The size of the buffer
+ *
+ * Return: A pointer to the result, or 0 if it cannot be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = 0, len2 = 0, len_res = 0, carry = 0;
-    int i = 0, j = 0;
-    char tmp;
+    int len1 = 0, len2 = 0, carry = 0, sum = 0;
+    int i, j;
 
     while (n1[len1] != '\0')
         len1++;
     while (n2[len2] != '\0')
         len2++;
 
-    len_res = (len1 > len2 ? len1 : len2) + 1;
-
-    if (len_res >= size_r)
+    if (len1 + 2 > size_r || len2 + 2 > size_r)
         return (0);
 
-    r[len_res] = '\0';
-    len_res--;
-
-    while (len1 > 0 || len2 > 0)
+    for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry == 1; i--, j--)
     {
-        tmp = carry;
-        if (len1 > 0)
-            tmp += n1[--len1] - '0';
-        if (len2 > 0)
-            tmp += n2[--len2] - '0';
-        carry = tmp / 10;
-        r[len_res--] = (tmp % 10) + '0';
+        sum = carry;
+        if (i >= 0)
+            sum += n1[i] - '0';
+        if (j >= 0)
+            sum += n2[j] - '0';
+        carry = sum / 10;
+        r[i + j + 1] = (sum % 10) + '0';
+    }
+    r[i + j + 2] = '\0';
+
+    for (i = 0, j = len1 + len2 - 1; i < j; i++, j--)
+    {
+        char tmp = r[i];
+        r[i] = r[j];
+        r[j] = tmp;
     }
 
-    if (carry)
-        r[len_res--] = carry + '0';
-
-    if (len_res < 0)
-        return (0);
-
-    return (r + len_res + 1);
+    return (r);
 }
