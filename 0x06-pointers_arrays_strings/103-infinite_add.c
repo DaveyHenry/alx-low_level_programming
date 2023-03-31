4d96851389c876e82efb783eb4e5976bf3514a91
David Henry Boakye-Yeboah <dhenryboakyeyeboah@gmail.com>
2023-03-30 18:45:24 -0700

    Fixing error

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * infinite _add - adds two numbers
+ * infinite_add - adds two numbers
  * @n1: first number
  * @n2: second number
  * @r: buffer for result
@@ -13,35 +13,46 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
 
+	/* Find lengths of n1 and n2 */
 	for (i = 0; n1[i]; i++)
 		;
 	for (j = 0; n2[j]; j++)
 		;
-	if (i > size_r || j > size_r)
+
+	/* Check if the sum can fit in r */
+	if (i > size_r - 1 || j > size_r - 1)
 		return (0);
+
+	/* Initialize variables */
 	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	k = 0;
+	i--;
+	j--;
+
+	/* Calculate the sum of the numbers */
+	while (i >= 0 || j >= 0 || m)
 	{
 		n = m;
 		if (i >= 0)
-			n += n1[i] - '0';
+			n += n1[i--] - '0';
 		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
+			n += n2[j--] - '0';
+
+		r[k++] = (n % 10) + '0';
 		m = n / 10;
-		r[k] = n % 10 + '0';
 	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
+
+	/* If r is full but there is still a carry, return 0 */
+	if (k == size_r && m)
 		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
+
+	/* Reverse r */
+	for (l = 0, k--; l < k; k--, l++)
 	{
 		m = r[k];
 		r[k] = r[l];
 		r[l] = m;
 	}
+
 	return (r);
 }
