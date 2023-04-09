debc064f5be6338ca86ded984c56869bc57f7d18
David Henry Boakye-Yeboah <dhenryboakyeyeboah@gmail.com>
2023-04-09 14:46:47 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,42 +1,16 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of pointers to arguments
- *
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
  * Return: Always 0 (Success)
  */
 int main(int argc, char **argv)
 {
-    _puts(argv[0]);
-    return (0);
-}
+	(void) argc;
+	printf("%s\n", argv[0]);
 
-/**
- * _puts - prints a string to stdout
- * @str: string to print
- *
- * Return: number of characters printed
- */
-int _puts(char *str)
-{
-    int i;
-
-    for (i = 0; str[i] != '\0'; i++)
-        _putchar(str[i]);
-
-    return (i);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
+	return (0);
 }
