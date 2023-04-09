7fd7c1ae4af09f798af2777ff0fffe6c27c2d8bd
David Henry Boakye-Yeboah <dhenryboakyeyeboah@gmail.com>
2023-04-09 15:09:15 -0700

    Fixing check errors

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * Return: void
- */
-void main(int argc, char *argv[])
+   * main - print the name of the program
+    * @argc: Count arguments
+     * @argv: Arguments
+      * Return: Always 0 (Success)
+*/
+int main(int argc, char **argv)
 {
-    printf("%i\n", argc - 1);
+	(void) argv; /*Ignore argv*/
+	printf("%i\n", argc - 1);
+
+	return (0);
 }
