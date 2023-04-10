8e1671ebf903911ea427f07d824ba286d2eed797
David Henry Boakye-Yeboah <dhenryboakyeyeboah@gmail.com>
2023-04-10 08:22:58 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
@@ -9,15 +10,15 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+        unsigned int i, j;
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		for (j = 0; *(accept + j) != '\0'; j++)
-		{
-			if (*(s + i) == *(accept + j))
-				return (s + i);
-		}
-	}
-	return (NULL);
+        for (i = 0; *(s + i) != '\0'; i++)
+        {
+                for (j = 0; *(accept + j) != '\0'; j++)
+                {
+                        if (*(s + i) == *(accept + j))
+                                return (s + i);
+                }
+        }
+        return (NULL);
 }
