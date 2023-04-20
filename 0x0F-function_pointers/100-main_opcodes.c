005ddb865f6013f8a168c94fa61a4c6cb7dc4855
David Henry Boakye-Yeboah <dhenryboakyeyeboah@gmail.com>
2023-04-20 04:49:47 -0700

    4. Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 /**
- * main - entry point
- * @argc: argument count
- * @argv: argument vector
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * Return: 0 on success, 1 if incorrect number of arguments, 2 if negative bytes
+ * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	char *opc = (char *) main;
 	int i, nbytes;
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "Error: incorrect number of arguments\n");
-		return (1);
+		printf("Error\n");
+		exit(1);
 	}
 
 	nbytes = atoi(argv[1]);
 
 	if (nbytes < 0)
 	{
-		fprintf(stderr, "Error: negative number of bytes\n");
-		return (2);
+		printf("Error\n");
+		exit(2);
 	}
 
 	for (i = 0; i < nbytes; i++)
