417a4c49aea0abd15eb36fdaf472e8e60f4192e2
DaveyHenry <dhenryboakyeyeboah@gmail.com>
2023-05-09 06:03:54 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,6 +12,7 @@
  *
  * Return: actual number of letters it could read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -20,45 +21,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
-	struct stat st;
-	if (fstat(fd, &st) == -1)
-	{
-		close(fd);
-		return (0);
-	}
-
-	size_t filesize = st.st_size;
-	if (letters > filesize)
-		letters = filesize;
-
-	buffer = malloc(sizeof(char) * (filesize + 1));
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
 	lenr = read(fd, buffer, letters);
 	close(fd);
-
 	if (lenr == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-
-	buffer[lenr] = '\0';
-
 	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-
-	if (lenw == -1 || (size_t) lenw != lenr)
+	if (lenr != lenw)
 		return (0);
-
 	return (lenw);
 }
